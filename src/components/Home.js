86a96b125022029bcf20140b625af9a88add1352
kaushiki_khabri <kaushiki.khabri@gmail.com>
2024-11-01 13:50:25 +0530

    Countdown added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Countdown from "./Countdown";
 
 function Home() {
+  const targetDate = "2025-01-30T23:59:59";
+
   return (
     <div>
       <Header />
+      <h1>Countdown Timer</h1>
+      <Countdown targetDate={targetDate} />
       <Footer />
     </div>
   );
