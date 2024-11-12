920bff7b0689c5cde233728b5ac2172f7652706a
Avasthi Jadhav <avasthijadhav14@gmail.com>
2024-11-12 18:42:30 +0530

    StarLineUp code commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import "./App.css";
+// import Home from "./components/Home";
+// import Launch from "./components/Launch";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/launch" element={<Launch />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Launch from "./components/Launch";
+import StarLineUp from "./components/StarLineUp";
 
 function App() {
   return (
@@ -11,6 +34,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/launch" element={<Launch />} />
+          <Route path="/star-lineup" element={<StarLineUp />} />
         </Routes>
       </Router>
     </div>
--- src/components/StarLineUp.js
+++ src/components/StarLineUp.js
@@ -1,8 +1,48 @@
-import React from "react";
-import "../style/StarLineUp.css";
+
+import React from 'react';
+import '../style/StarLineUp.css';
+
+const shows = [
+  {
+    name: 'Sabali Band',
+    description: 'Experience the rhythm and soul with Sabali Band.',
+    image: require('../images/sabali.jpg'),
+  },
+  {
+    name: 'Stand-up Comedy Night',
+    description: 'Get ready to laugh out loud with top comedians!',
+    image: require('../images/comedy.jpg'),
+  },
+  {
+    name: 'DJ Night',
+    description: 'Dance to the best beats under the stars!',
+    image: require('../images/dj.jpeg'),
+  }
+  //,
+  // {
+  //   name: 'Acoustic Evening',
+  //   description: 'Unplug and unwind with soulful acoustic tunes.',
+  //   image: require('../images/acoustic.jpg'),
+  // },
+];
 
 function StarLineUp() {
-  return <div>StarLineUp</div>;
+  return (
+    <div className="star-lineup-container">
+      <h2 className="title">Star Line-Up</h2>
+      <div className="lineup-grid">
+        {shows.map((show, index) => (
+          <div className="show-card" key={index}>
+            <img src={show.image} alt={show.name} className="show-image" />
+            <div className="show-info">
+              <h3>{show.name}</h3>
+              <p>{show.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default StarLineUp;
