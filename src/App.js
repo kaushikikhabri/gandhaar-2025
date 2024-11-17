e99ff595bea6b55f3e89e08fda3b72bd425cde94
sameekshatantak <148996308+sameekshatantak@users.noreply.github.com>
2024-11-17 17:28:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import EventDetails from "./components/EventDetails";
 import Home from "./components/Home";
 import Launch from "./components/Launch";
+// Updated import paths for All-Events folder
+import ClassicalDance from "./components/All-Events/ClassicalDance";
+import IntraCollegeDance from "./components/All-Events/IntraCollegeDance";
+import SoloDance from "./components/All-Events/SoloDance";
+import StreetGroove from "./components/All-Events/StreetGroove";
 
 function App() {
   return (
@@ -11,6 +17,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/launch" element={<Launch />} />
+          <Route path="/event-details" element={<EventDetails />} />
+          <Route path="/street-groove" element={<StreetGroove />} />
+          <Route path="/solo-dance" element={<SoloDance />} />
+          <Route path="/intra-college-dance" element={<IntraCollegeDance />} />
+          <Route path="/classical-dance" element={<ClassicalDance />} />
         </Routes>
       </Router>
     </div>
