433cced79442d0b365f59813f1c52760c60f6815
sameekshatantak <148996308+sameekshatantak@users.noreply.github.com>
2024-11-17 17:50:28 +0530

    Update Events.js

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../style/Events.css";
 
 function Events() {
-  return <div>Events</div>;
+  return (
+    <div>
+      {/* Button to navigate to EventDetails page */}
+      <Link to="/event-details">
+        <button className="navigate-button">Go to Event Details</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Events;
+
