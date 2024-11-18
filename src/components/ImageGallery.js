b8837434f74e8a87665318d73d8359b981398a41
Yashoda2003 <116747256+Yashoda2003@users.noreply.github.com>
2024-11-18 21:16:24 +0530

    updated ImageGallery changes

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from 'react';
 import "../style/ImageGallery.css";
-import image1 from '../images/image1.jpeg';
-import image2 from '../images/image2.jpg';
-import image3 from '../images/image3.jpeg';
-import image4 from '../images/image4.jpg';
-import image5 from '../images/image5.jpeg';
-import image6 from '../images/image6.jpeg';
-import image7 from '../images/image7.jpeg';
-import image8 from '../images/image8.jpeg';
-import image9 from '../images/image9.jpeg';
-import image10 from '../images/image10.jpeg';
-import image11 from '../images/image11.jpeg';
+import image1 from '../images/image1.JPG';
+import image2 from '../images/image2.JPG';
+import image3 from '../images/image3.JPG';
+import image4 from '../images/image4.JPG';
+import image5 from '../images/image5.JPG';
+import image6 from '../images/image6.JPG';
+import image7 from '../images/image7.JPG';
+import image8 from '../images/image8.JPG';
+import image9 from '../images/image9.JPG';
+import image10 from '../images/image10.JPG';
+import image11 from '../images/image11.JPG';
+import image12 from '../images/image12.JPG';
+import image13 from '../images/image13.JPG';
+import image14 from '../images/image14.JPG';
 
 const images = [
   { src: image1, alt: 'Image 1' },
@@ -24,43 +27,74 @@ const images = [
   { src: image9, alt: 'Image 9' },
   { src: image10, alt: 'Image 10' },
   { src: image11, alt: 'Image 11' },
+  { src: image12, alt: 'Image 12' },
+  { src: image13, alt: 'Image 13' },
+  { src: image14, alt: 'Image 14' },
 ];
 
-function ImageGallery() {
-  const [selectedImage, setSelectedImage] = useState(null);
+ 
+const ImageGallery = () => {
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+  const imageGridRef = useRef(null);
 
-  const openModal = (image) => {
-    setSelectedImage(image);
+  const openModal = (index) => {
+    setSelectedImageIndex(index);
+    if (imageGridRef.current) {
+      imageGridRef.current.style.animationPlayState = 'paused';
+    }
   };
 
   const closeModal = () => {
-    setSelectedImage(null);
+    setSelectedImageIndex(null);
+    if (imageGridRef.current) {
+      imageGridRef.current.style.animationPlayState = 'running';
+    }
+  };
+
+  const showPrevImage = () => {
+    setSelectedImageIndex((prevIndex) => 
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const showNextImage = () => {
+    setSelectedImageIndex((prevIndex) => 
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   return (
     <div className="gallery-container">
-      {/* <div className="scrolling-text">GANDHAAR-2024</div> */}
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div key={index} className="image-item" onClick={() => openModal(image)}>
+      <div className="image-grid" ref={imageGridRef}>
+        {[...images].map((image, index) => (
+          <div 
+            key={index} 
+            className="image-item" 
+            style={{
+              transform: `translateY(${index % 2 === 0 ? -20 : 10}px)`,
+            }} 
+            onClick={() => openModal(index % images.length)}
+          >
             <img src={image.src} alt={image.alt} />
           </div>
         ))}
       </div>
-      {/* <div className="scrolling-text">GANDHAAR-2024</div> */}
-
-      {/* Modal for displaying the full-size image */}
-      {selectedImage && (
+      {selectedImageIndex !== null && (
         <div className="modal" onClick={closeModal}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <span className="close" onClick={closeModal}>&times;</span>
-            <img src={selectedImage.src} alt={selectedImage.alt} className="modal-image" />
+            <img 
+              src={images[selectedImageIndex].src} 
+              alt={images[selectedImageIndex].alt} 
+              className="modal-image" 
+            />
+            <span className="arrow left" onClick={showPrevImage}>&#10094;</span>
+            <span className="arrow right" onClick={showNextImage}>&#10095;</span>
           </div>
         </div>
       )}
     </div>
   );
-
-}
+};
 
 export default ImageGallery;
