8dd9bf644344332a84405feca81573de2c860cbd
Yashoda2003 <116747256+Yashoda2003@users.noreply.github.com>
2024-12-01 21:49:50 +0530

    Merge branch 'yashoda-ImageGallery' of https://github.com/kaushikikhabri/gandhaar-2025 into yashoda-ImageGallery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import "./App.css";
+// import Home from "./components/Home";
+// import Launch from "./components/Launch";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/launch" element={<Launch />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import EventDetails from "./components/EventDetails";
 import Home from "./components/Home";
 import Launch from "./components/Launch";
+import ClassicalDance from "./components/All-Events/ClassicalDance";
+import IntraCollegeDance from "./components/All-Events/IntraCollegeDance";
+import SoloDance from "./components/All-Events/SoloDance";
+import StreetGroove from "./components/All-Events/StreetGroove";
+import StarLineUp from "./components/StarLineUp";
 
 function App() {
   return (
@@ -11,6 +39,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/launch" element={<Launch />} />
+          <Route path="/event-details" element={<EventDetails />} />
+          <Route path="/street-groove" element={<StreetGroove />} />
+          <Route path="/solo-dance" element={<SoloDance />} />
+          <Route path="/intra-college-dance" element={<IntraCollegeDance />} />
+          <Route path="/classical-dance" element={<ClassicalDance />} />
+          <Route path="/star-lineup" element={<StarLineUp />} />
         </Routes>
       </Router>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Launch from "./Launch";
 import Header from "./Header";
+import Navigation from "./Navigation";
 import Countdown from "./Countdown";
 import PreGandhaarThemes from "./PreGandhaarThemes";
 import GandhaarThemes from "./GandhaarThemes";
@@ -9,17 +10,26 @@ import StarLineUp from "./StarLineUp";
 import Schedule from "./Schedule";
 import ImageGallery from "./ImageGallery";
 import Footer from "./Footer";
+import Event from "./Event";
+import EventSlider from "./EventSlider";
+import EventCard from "./EventCard";
+import CulturalFestMindMap from "./CulturalFestMindMap";
 
 function Home() {
   const targetDate = "2025-01-30T23:59:59";
   return (
     <div>
       <Header />
+      <Navigation />
       <Countdown targetDate={targetDate} />
       <PreGandhaarThemes />
-      <GandhaarThemes />
-      <Events />
+      <CulturalFestMindMap />
+      {/* <GandhaarThemes /> */}
+      {/* <Events /> */}
+      <Event />
+      <EventSlider />
       <StarLineUp />
+      <EventCard />
       <Schedule />
       <ImageGallery />
       <Footer />
