30842137bf7362431559747f16fa5fc9695821fb
unknown <sameeksha.tantak@cumminscollege.in>
2024-12-07 19:09:29 +0530

    Updated event details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import "./App.css";
+// import Home from "./components/Home";
+// import Launch from "./components/Launch";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/launch" element={<Launch />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import EventDetails from "./components/EventDetails";
 import Home from "./components/Home";
 import Launch from "./components/Launch";
-import StarLineUp from "./components/StarLineUp";
-
+// Updated import paths for All-Events folder
+import AllEvents from "./components/AllEvents";
 function App() {
   return (
     <div className="App">
@@ -12,7 +34,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/launch" element={<Launch />} />
-          <Route path="/star-lineup" element={<StarLineUp />} />
+          <Route path="/event-details" element={<EventDetails />} />
+
+              {/* Use eventKey to identify the event */}
+        <Route path="/miss-freshers" element={<AllEvents eventKey="missFreshers" />} />
+        <Route path="/dance-battle" element={<AllEvents eventKey="danceBattle" />} />
         </Routes>
       </Router>
     </div>
