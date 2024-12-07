539a11a2fb87ba4b9199ff3cd445d3131926ca56
Isha Bule <ishabule753@gmail.com>
2024-12-07 17:34:00 +0530

    navbar update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style/Home.css";
 import Launch from "./Launch";
 import Header from "./Header";
@@ -20,23 +20,28 @@ import DummyGallery from "./DummyGallery";
 
 function Home() {
   const targetDate = "2025-01-30T23:59:59";
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen((prev) => !prev);
+  };
   return (
     <div>
-      <Header />
-      <Navigation />
+      <Header toggleDrawer={toggleDrawer} />
+      <Navigation isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
       <Countdown targetDate={targetDate} />
-      <PreGandhaarThemes />
-      <CulturalFestMindMap />
+      {/* <PreGandhaarThemes /> */}
+      {/* <CulturalFestMindMap /> */}
       {/* <GandhaarThemes /> */}
       {/* <Events /> */}
-      <HundredDaysOfPoetry />
-      <Event />
-      <DummyGallery />
+      {/* <HundredDaysOfPoetry /> */}
+      {/* <Event /> */}
+      {/* <DummyGallery /> */}
       {/* <EventSlider /> */}
-      <StarLineUp />
+      {/* <StarLineUp />
       <EventCard />
       <Schedule />
-      <ImageGallery />
+      <ImageGallery /> */}
       <Footer />
     </div>
   );
