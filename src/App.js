27a86487eba97c4f7b1476ad351890fbd8a9d711
srushti-nikam <srushti.v.nikam@cumminscollege.in>
2024-12-10 19:20:57 +0530

    music map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
+// App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
-import Launch from "./components/Launch";
+import CulturalFestMindMap from "./CulturalFestMindMap";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/launch" element={<Launch />} />
-        </Routes>
-      </Router>
+      <CulturalFestMindMap />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import CulturalFestMindMap from "./CulturalFestMindMap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CulturalFestMindMap />
   </React.StrictMode>
 );
 
