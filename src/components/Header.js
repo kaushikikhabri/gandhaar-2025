eed8a3df195ee4c3539f95d9679d89cb1d29905d
Isha Bule <ishabule753@gmail.com>
2024-12-12 17:00:53 +0530

    css update for nav header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import s_logo from "../images/sanstha-logo.png";
-import c_logo from "../images/college-logo.png";
+import s_logo from "../images/white_sanstha_logo.png";
+import c_logo from "../images/white_college_logo.png";
 import "../style/Header.css";
 
 const Header = ({ toggleDrawer }) => {
@@ -27,18 +27,20 @@ const Header = ({ toggleDrawer }) => {
           <li className="nav-item"><a href="#home">Home</a></li>
           <li className="nav-item"><a href="#events">Events</a></li>
           <li className="nav-item"><a href="#schedule">Schedule</a></li>
-          <li className="nav-item"><a href="#workshops">Workshops</a></li>
+          <li className="nav-item"><a href="#theme">Theme</a></li>
+          <li className="nav-item"><a href="#star-line-up">Star-Lineup</a></li>
           <li className="nav-item"><a href="#team">Team</a></li>
+          <li className="nav-item"><a href="#gallery">Gallery</a></li>
           <li className="nav-item"><a href="#about-us">About Us</a></li>
+
         </ul>
         <div className="logo-right">
           <img id="logo2" src={c_logo} alt="College Logo" />
         </div>
       </div>
       <div className="tag">
-        <p className="tag1">MKSSS's Cummins College of Engineering for Women</p>
-        <p className="tag2">Brings You</p>
-        <p className="tag3">Gandhaar 2025</p>
+        <p className="tag1">MKSSS's Cummins College of Engineering for Women, Pune</p>
+        <p className="tag3"><span>GANDHAAR</span> <span>2025</span></p>
       </div>
       {isMobile && (
         <div className="floating-icon" onClick={()=> toggleDrawer(true)}>
