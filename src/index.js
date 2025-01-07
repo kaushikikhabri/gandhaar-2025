4e30dc6bb5fab1379179c5c54466492d591ae7c5
srushti-nikam <srushti.v.nikam@cumminscollege.in>
2025-01-07 23:00:21 +0530

    updated themes and teams

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import Team from "./components/Team";
+import CulturalFestMindMap from "./components/CulturalFestMindMap";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Team />
+    <CulturalFestMindMap />
   </React.StrictMode>
 );
 
