dc5c07c11e81fd62dafbe20c2d31c8776aaa03fc
Yashoda2003 <116747256+Yashoda2003@users.noreply.github.com>
2025-01-08 18:11:22 +0530

    updated event_details

--- src/components/event_details.js
+++ src/components/event_details.js
@@ -3,7 +3,7 @@ const event_details = {
     start: "12:30 PM",
     end: "6:00 PM",
     location: "Main Bldg Quadrangle",
-    category: "Dance",
+    category: "Dance (GROOVE ARENA)",
     day: "Jan 29",
     teamSize: 19,
     imageUrl:
@@ -13,7 +13,7 @@ const event_details = {
     start: "11:00 AM",
     end: "2:00 PM",
     location: "Stage 2",
-    category: "Dance",
+    category: "Dance (GROOVE ARENA)",
     day: "Jan 31",
     teamSize: 1,
     imageUrl:
@@ -23,7 +23,7 @@ const event_details = {
     start: "10:00 AM",
     end: "1:00 PM",
     location: "Main Bldg Quadrangle",
-    category: "Dance",
+    category: "Dance (GROOVE ARENA)",
     day: "Feb 1",
     teamSize: 5,
     imageUrl:
@@ -33,7 +33,7 @@ const event_details = {
     start: "3:00 PM",
     end: "5:00 PM",
     location: "Stage 2",
-    category: "Dance",
+    category: "Dance (GROOVE ARENA)",
     day: "Jan 30",
     teamSize: 1,
     imageUrl:
@@ -43,12 +43,206 @@ const event_details = {
     start: "10:00 AM",
     end: "12:30 PM",
     location: "Main Bldg Quadrangle",
-    category: "Dance",
+    category: "Dance (GROOVE ARENA)",
     day: "Jan 30",
     teamSize: 15,
     imageUrl:
       "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
   },
+   "Sargam Symphony": {
+    start: "10:00 AM",
+    end: "1:30 PM",
+    location: "Main Bldg Quadrangle",
+    category: "Music (JAMSPHERE)",
+    day: "Jan 31",
+    teamSize: 2,
+    imageUrl:
+       "https://media.licdn.com/dms/image/v2/D5612AQFRw-MO07-Y4g/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1710541180651?e=1741824000&v=beta&t=vwx7Ae6WKbzg-bkkh4uP4rGpjQBqmi-3KBL2EkPMT1M",
+  },
+   "Suron ke Sikandar": {
+    start: "10:00 AM",
+    end: "12:00 PM",
+    location: "KB Joshi Audi",
+    category: "Fun (GATHERING HUB)",
+    day: "Jan 30",
+    teamSize: 1,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/colorful-microphone-black-background-generative-ai_918839-1265.jpg?w=900",
+  },
+  "The Great Faculty Sing-Off": {
+    start: "2:00 PM",
+    end: "4:00 PM",
+    location: "Instru Audi",
+    category: "Fun (GATHERING HUB)",
+    day: "Jan 29",
+    teamSize: 1,
+    imageUrl:
+      "https://thumbs.dreamstime.com/b/vivid-illustration-microphone-musical-elements-ai-generated-vivid-illustration-microphone-musical-elements-335216799.jpg?w=992",
+  },
+  "CUMMINS GOT LATENT": {
+    start: "12:00 PM",
+    end: "2:30 PM",
+    location: "Stage 2",
+    category: "Fun (GATHERING HUB)",
+    day: "Jan 30",
+    teamSize: 1,
+    imageUrl:
+      "https://www.rawpixel.com/image/14056921/magic-magician-light-night",
+  },
+  "Shantit Kranti": {
+    start: "3:00 PM",
+    end: "5:00 PM",
+    location: "KB Joshi Audi",
+    category: "Fun (GATHERING HUB)",
+    day: "Jan 29",
+    teamSize: 6,
+    imageUrl:
+      "https://t4.ftcdn.net/jpg/10/85/32/61/240_F_1085326141_29OFQptRbrrVt2mLuLRsiEzwtJmhAFb2.jpg",
+  },
+  "KALAKAR KATTA": {
+    start: "11:00 AM",
+    end: "5:00 PM",
+    location: "Instrumentation Quadrangle",
+    category: "Fun (GATHERING HUB)",
+    day: "Jan 31",
+    teamSize: "-",
+    imageUrl:
+      "https://img.freepik.com/premium-photo/vibrant-colors-celebrate-beauty-creativity-painted-women-portrait-generated-by-artificial-intelligence_24640-123697.jpg?w=900",
+  },
+  "ESCAPE ROOM": {
+    start: "11:00 AM",
+    end: "1:00 PM",
+    location: "Entire IT/Mech Building",
+    category: "Fun (GATHERING HUB)",
+    day: "Jan 31",
+    teamSize: 4,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/inclusive-pridethemed-escape-room-experience-generative-ai_883586-207229.jpg",
+  },
+  "TALAASH": {
+    start: "10:30 AM",
+    end: "12:30 PM",
+    location: "All Campus",
+    category: "Fun (GATHERING HUB)",
+    day: "Feb 1",
+    teamSize: 4,
+    imageUrl:
+      "https://riddlepedia.com/wp-content/uploads/2024/02/prom-scavenger-hunt-riddles.png",
+  },
+  "HAUTEAURA": {
+    start: "6:00 PM",
+    end: "9:00 PM",
+    location: "Samstha Ground",
+    category: "Runway (HAUTE ROUTE)",
+    day: "Jan 29",
+    teamSize: "18 - 21 students",
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Miss Cummins-Beauty Contest": {
+    start: "2:00 PM",
+    end: "4:30 PM",
+    location: "Main Building Quadrangle",
+    category: "Runway (HAUTE ROUTE)",
+    day: "Jan 31",
+    teamSize: "18 - 21 students (14-17 performers + 4 helpers)",
+    imageUrl:
+      "https://img.freepik.com/premium-photo/contestants-stunning-evening-gowns-walking-runway-beauty-pageant_1284400-6615.jpg",
+  },
+  "Eco Vogue": {
+    start: "1:00 PM",
+    end: "3:00 PM",
+    location: "Main Building Quadrangle",
+    category: "Runway (HAUTE ROUTE)",
+    day: "Jan 30",
+    teamSize: 3,
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Goonj 3.0": {
+    start: "2:30 PM",
+    end: "4:30 PM",
+    location: "Stage 2",
+    category: "Literary (WORDSMITH’S CORNER)",
+    day: "Jan 29",
+    teamSize: 1,
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Lights, Quiz, Action!": {
+    start: "2:00 PM",
+    end: "4:00 PM",
+    location: "KB Joshi Audi",
+    category: "Literary (WORDSMITH’S CORNER)",
+    day: "Jan 31",
+    teamSize: 5,
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Spellbound": {
+    start: "12:30 PM",
+    end: "2:30 PM",
+    location: "KB Joshi Audi",
+    category: "Literary (WORDSMITH’S CORNER)",
+    day: "Jan 29",
+    teamSize: "2-3 members",
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Mic in the Open": {
+    start: "3:00 PM",
+    end: "5:00 PM",
+    location: "Stage 2",
+    category: "Literary (WORDSMITH’S CORNER)",
+    day: "Jan 31",
+    teamSize: 1,
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Taste Wars": {
+    start: "10:00 AM",
+    end: "2:00 PM",
+    location: "Between Main & IT Building",
+    category: "Literary (WORDSMITH’S CORNER)",
+    day: "Jan 31",
+    teamSize: "2 members (minimum) / 4 members (maximum)",
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Kicks and Dyes Showdown": {
+    start: "12:30 PM",
+    end: "2:30 PM",
+    location: "Instru Quadrangle",
+    category: "Artistry (ARTISCOPE)",
+    day: "Jan 29",
+    teamSize: "1 or 2",
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  "Hands On Henna": {
+    start: "3:30 PM",
+    end: "5:30 PM",
+    location: "Mech Circle",
+    category: "Artistry (ARTISCOPE)",
+    day: "Jan 29",
+    teamSize: "1 (Self- artist)/ 2 (1 artist + 1 model)",
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+
+  // have to add events of Artistry (ARTISCOPE) category
+
+  "The Drama Premiere League": {
+    start: "3:30 PM",
+    end: "5:30 PM",
+    location: "Mech Circle",
+    category: "Artistry (ARTISCOPE)",
+    day: "Jan 29",
+    teamSize: "1 (Self- artist)/ 2 (1 artist + 1 model)",
+    imageUrl:
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
+  },
+  
 };
 
 export default event_details;
