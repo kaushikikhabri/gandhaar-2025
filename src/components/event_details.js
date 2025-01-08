4c7a0343743a6404de9117048a1992c5e39fd36b
kaushiki_khabri <kaushiki.khabri@gmail.com>
2025-01-08 15:20:25 +0530

    avasthi updated

--- src/components/event_details.js
+++ src/components/event_details.js
@@ -1,133 +1,53 @@
 const event_details = {
-  inauguration: {
-    start: "10:00 AM",
-    end: "11:00 AM",
+  grooveArena: {
+    start: "12:30 PM",
+    end: "6:00 PM",
     location: "Main Bldg Quadrangle",
-    category: "Fun",
-    day: "Feb 1",
-    teamSize: 1,
+    category: "Dance",
+    day: "Jan 29",
+    teamSize: 19,
     imageUrl:
-      "https://www.holidify.com/images/cmsuploads/compressed/27858916_1744144345636773_5809530212233948261_n_20180410145639.jpg",
+      "https://media.istockphoto.com/id/1257938447/vector/musical-summer-dance-festival-vector-illustration-of-musicians-dancers-disco-dancing-people.jpg?s=1024x1024&w=is&k=20&c=xmrxYB18DSyXYtqkwEXA6U1je0eKQRt6VOU3D9SGNQg=",
   },
-  zumba: {
+  soloSynergy: {
     start: "11:00 AM",
-    end: "12:00 PM",
+    end: "2:00 PM",
     location: "Stage 2",
-    category: "Workshop",
-    day: "Feb 1",
+    category: "Dance",
+    day: "Jan 31",
     teamSize: 1,
     imageUrl:
       "https://media.istockphoto.com/id/858777608/vector/dance-battle-party-poster.jpg?s=612x612&w=0&k=20&c=Adk3VCE61K6aQ4SsE_EMOImIY_ByTkhY_WZps8c2s_U=",
   },
-  dilSeDance: {
-    start: "12:00 PM",
+  nrutyanjali: {
+    start: "10:00 AM",
     end: "1:00 PM",
     location: "Main Bldg Quadrangle",
     category: "Dance",
     day: "Feb 1",
-    teamSize: 9,
-    imageUrl:
-      "https://media.istockphoto.com/id/1257938447/vector/musical-summer-dance-festival-vector-illustration-of-musicians-dancers-disco-dancing-people.jpg?s=1024x1024&w=is&k=20&c=xmrxYB18DSyXYtTNm9BIQoHVEAHIHwlT854rLQszMTA=",
-  },
-  sketchSaga: {
-    start: "1:00 PM",
-    end: "2:00 PM",
-    location: "Between Main & IT Building",
-    category: "Artistry",
-    day: "Feb 1",
-    teamSize: 2,
+    teamSize: 5,
     imageUrl:
       "https://www.holidify.com/images/cmsuploads/compressed/27858916_1744144345636773_5809530212233948261_n_20180410145639.jpg",
   },
-  facePainting: {
-    start: "2:00 PM",
-    end: "3:00 PM",
-    location: "Mech Circle",
-    category: "Artistry",
-    day: "Feb 1",
-    teamSize: 2,
-    imageUrl:
-      "https://www.shutterstock.com/image-vector/music-festival-banner-girls-singing-260nw-2231907083.jpg",
-  },
-  adMad: {
+  streetBrawl: {
     start: "3:00 PM",
-    end: "4:00 PM",
-    location: "Stage 2",
-    category: "Drama",
-    day: "Feb 2",
-    teamSize: 4,
-    imageUrl:
-      "https://media.istockphoto.com/id/1257938447/vector/musical-summer-dance-festival-vector-illustration-of-musicians-dancers-disco-dancing-people.jpg?s=1024x1024&w=is&k=20&c=xmrxYB18DSyXYtTNm9BIQoHVEAHIHwlT854rLQszMTA=",
-  },
-  suronKaSafar: {
-    start: "4:00 PM",
     end: "5:00 PM",
-    location: "Instru Audi",
-    category: "Music",
-    day: "Feb 2",
-    teamSize: 2,
-    imageUrl:
-      "https://www.holidify.com/images/cmsuploads/compressed/27858916_1744144345636773_5809530212233948261_n_20180410145639.jpg",
-  },
-  heGele: {
-    start: "5:00 PM",
-    end: "6:00 PM",
-    location: "KB Joshi Hall",
-    category: "Drama",
-    day: "Feb 2",
-    teamSize: 6,
-    imageUrl:
-      "https://www.shutterstock.com/image-vector/music-festival-banner-girls-singing-260nw-2231907083.jpg",
-  },
-  insia: {
-    start: "6:00 PM",
-    end: "7:00 PM",
     location: "Stage 2",
     category: "Dance",
-    day: "Feb 2",
-    teamSize: 12,
+    day: "Jan 30",
+    teamSize: 1,
     imageUrl:
-      "https://media.istockphoto.com/id/1257938447/vector/musical-summer-dance-festival-vector-illustration-of-musicians-dancers-disco-dancing-people.jpg?s=1024x1024&w=is&k=20&c=xmrxYB18DSyXYtTNm9BIQoHVEAHIHwlT854rLQszMTA=",
+      "https://media.istockphoto.com/id/971376146/vector/dance-off.jpg?s=612x612&w=0&k=20&c=6bgzDGsIG5L06ShVUBzWbHtQoI5x3Sk8H67B9aXciCE=",
   },
-  doodle: {
-    start: "7:00 PM",
-    end: "8:00 PM",
-    location: "Between Main & IT Building",
-    category: "Artistry",
-    day: "Feb 3",
-    teamSize: 2,
+  stepItUp: {
+    start: "10:00 AM",
+    end: "12:30 PM",
+    location: "Main Bldg Quadrangle",
+    category: "Dance",
+    day: "Jan 30",
+    teamSize: 15,
     imageUrl:
-      "https://www.holidify.com/images/cmsuploads/compressed/27858916_1744144345636773_5809530212233948261_n_20180410145639.jpg",
-  },
-  vaadVivad: {
-    start: "8:00 PM",
-    end: "9:00 PM",
-    location: "Stage 2",
-    category: "Drama",
-    day: "Feb 3",
-    teamSize: 4,
-    imageUrl:
-      "https://www.shutterstock.com/image-vector/music-festival-banner-girls-singing-260nw-2231907083.jpg",
-  },
-  heenaHustle: {
-    start: "9:00 PM",
-    end: "10:00 PM",
-    location: "Mech Circle",
-    category: "Artistry",
-    day: "Feb 3",
-    teamSize: 2,
-    imageUrl:
-      "https://media.istockphoto.com/id/1257938447/vector/musical-summer-dance-festival-vector-illustration-of-musicians-dancers-disco-dancing-people.jpg?s=1024x1024&w=is&k=20&c=xmrxYB18DSyXYtTNm9BIQoHVEAHIHwlT854rLQszMTA=",
-  },
-  fashionShow: {
-    start: "10:00 PM",
-    end: "11:00 PM",
-    location: "Samastha Ground",
-    category: "Runway",
-    day: "Feb 4",
-    teamSize: 16,
-    imageUrl:
-      "https://www.holidify.com/images/cmsuploads/compressed/27858916_1744144345636773_5809530212233948261_n_20180410145639.jpg",
+      "https://media.istockphoto.com/id/937674774/vector/abstract-modern-music-poster-template.jpg?s=612x612&w=0&k=20&c=8WTeLpUCJRWadbiQ_9cVSfjSa2x-CVOSG5M5FbYmJ9w=",
   },
 };
 
